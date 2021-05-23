79414dce1f05e3d5a8dc20875893abf7d214652f
Xandrak <dustin.segers@gmail.com>
2021-05-23 15:20:41 -0500

    Extract out Course.Status type

---
+++ src/Types/Course/Status.hs
@@ -0,0 +1,29 @@
+module Types.Course.Status where
+
+import Data.Aeson
+import qualified Data.Text as Text
+
+data Status = Scheduled | InProduction | Available
+  deriving (Show)
+
+instance ToJSON Status where
+  toJSON status = toJSON $ statusToText status
+
+statusToText :: Status -> Text.Text
+statusToText status = case status of
+  Scheduled -> "scheduled"
+  InProduction -> "in_production"
+  Available -> "available"
+
+instance FromJSON Status where
+  parseJSON = withText "Status" $ \txt ->
+    case textToStatus txt of
+      Right s -> pure s
+      Left err -> undefined
+
+textToStatus :: Text.Text -> Either Text.Text Status
+textToStatus text = case text of
+  "scheduled" -> Right Scheduled
+  "in_production" -> Right InProduction
+  "available" -> Right Available
+  _ -> Left "Invalid status"
