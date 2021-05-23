18765cf01fa057078b88a99cde8d523d927928cd
Xandrak <dustin.segers@gmail.com>
2021-05-23 15:53:08 -0500

    Setup Hspec and basic pending test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+module Spec where
+
+import qualified Control.Monad.IO.Class as IO
+import qualified Test.Hspec as Test
+import qualified Web.Scotty as Scotty
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = 
+  Test.hspec $ do
+    Test.describe "Thing" $ do
+      Test.it "returns thing" $ 
+        Test.pending
