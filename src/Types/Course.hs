b4b6f305b78b1f71d020127afc650352ea311594
Xandrak <dustin.segers@gmail.com>
2021-05-23 15:21:12 -0500

    Extract out Course data type

---
+++ src/Types/Course.hs
@@ -0,0 +1,18 @@
+module Types.Course where
+
+import GHC.Generics
+import Data.Aeson
+import qualified Data.Time as Time
+import qualified Types.Course.Status as Status
+
+data Course = Course
+  { id :: Int,
+    name :: String,
+    status :: Status.Status,
+    createdAt :: Time.UTCTime
+  }
+  deriving (Generic, Show)
+
+instance ToJSON Course
+
+instance FromJSON Course
